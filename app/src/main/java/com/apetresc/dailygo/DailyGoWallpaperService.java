3d597966531e0b115a2f57ac72b1b38edbb199d7
Adrian Petrescu <apetresc@gmail.com>
2016-03-19 00:55:19 -0700

    Register wallpaper activity in AndroidManifest.xml
    It can now actually be selected and used from the wallpaper
    picker (though it still does nothing)

--- app/src/main/java/com/apetresc/dailygo/DailyGoWallpaperService.java
+++ app/src/main/java/com/apetresc/dailygo/DailyGoWallpaperService.java
@@ -2,8 +2,12 @@ package com.apetresc.dailygo;
 
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 public class DailyGoWallpaperService extends WallpaperService {
@@ -13,6 +17,7 @@ public class DailyGoWallpaperService extends WallpaperService {
     }
 
     private class DailyGoWallpaperEngine extends Engine implements SharedPreferences.OnSharedPreferenceChangeListener {
+        private final Handler handler = new Handler();
         private final Runnable drawRunner = new Runnable() {
             @Override
             public void run() {
@@ -21,20 +26,59 @@ public class DailyGoWallpaperService extends WallpaperService {
         };
 
         private Paint paint = new Paint();
+        private boolean visible = true;
+        private int width;
+        private int height;
 
         @Override
         public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
 
         }
 
+        public DailyGoWallpaperEngine() {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(DailyGoWallpaperService.this);
+            prefs.registerOnSharedPreferenceChangeListener(this);
+            handler.post(drawRunner);
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            this.visible = visible;
+            if (visible) {
+                handler.post(drawRunner);
+            } else {
+                handler.removeCallbacks(drawRunner);
+            }
+        }
+
+        @Override
+        public void onSurfaceDestroyed(SurfaceHolder holder) {
+            super.onSurfaceDestroyed(holder);
+            this.visible = false;
+            handler.removeCallbacks(drawRunner);
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(DailyGoWallpaperService.this);
+            pref.unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSurfaceChanged(SurfaceHolder holder, int format,
+                                     int width, int height) {
+            this.width = width;
+            this.height = height;
+            super.onSurfaceChanged(holder, format, width, height);
+        }
+
         private void draw() {
+            Log.d("DailyGo", "In draw!");
             final SurfaceHolder holder = getSurfaceHolder();
             Canvas canvas = null;
 
             try {
                 canvas = holder.lockCanvas();
                 if (canvas != null) {
-                    canvas.drawText("Hello world", 50, 50, paint);
+                    paint.setColor(Color.WHITE);
+                    paint.setTextSize(50);
+                    canvas.drawText("Hello world", 550, 550, paint);
                 }
             } finally {
                 if (canvas != null) {
